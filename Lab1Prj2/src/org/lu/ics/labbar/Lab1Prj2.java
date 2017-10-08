961cf2c10a9e13affa851e896184dd96b28620e5
Anton Laptop <anton.hejman@gmail.com>
2017-10-09 00:40:51 +0200

    1.2

--- Lab1Prj2/src/org/lu/ics/labbar/Lab1Prj2.java
+++ Lab1Prj2/src/org/lu/ics/labbar/Lab1Prj2.java
@@ -7,13 +7,14 @@ public class Lab1Prj2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	      Scanner inPut = new Scanner(System.in);
+	      
 	      System.out.println("Vad heter du i förnamn? ");
 	      String firstName = inPut.nextLine() ;
 	      
 	      System.out.println("Vad heter du i efternamn? ");
 	      String lastName = inPut.nextLine();
 	      
-	      System.out.println("Du heter alltså: " + lastName + firstName );
+	      System.out.println("Du heter alltså: " + lastName + " " + firstName );
 	      
 	      inPut.close();
 	      
