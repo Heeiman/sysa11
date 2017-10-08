eff4781d96539a69cc60bff0dc62efd532622949
Anton <anton.hejman@gmail.com>
2017-10-09 00:21:48 +0200

    1.0

---
+++ Lab1Prj2/src/org/lu/ics/labbar/Lab1Prj2.java
@@ -0,0 +1,24 @@
+package org.lu.ics.labbar;
+
+import java.util.Scanner;
+
+public class Lab1Prj2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      Scanner inPut = new Scanner(System.in);
+	      System.out.println("Vad heter du i förnamn? ");
+	      String firstName = inPut.nextLine() ;
+	      
+	      System.out.println("Vad heter du i efternamn? ");
+	      String lastName = inPut.nextLine();
+	      
+	      System.out.println("Du heter alltså: " + lastName + firstName );
+	      
+	      inPut.close();
+	      
+	      		  
+	        
+	}
+
+}
